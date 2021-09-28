c5b3597f14081443cc0d33adbbc41bd7aea3f998
21seu <1693376841@qq.com>
2021-09-28 23:32:52 +0800

    springboot整合jwt

---
+++ 02_spb_jwt/src/main/java/com/ftj/entity/User.java
@@ -0,0 +1,16 @@
+package com.ftj.entity;
+
+import lombok.Data;
+
+/**
+ * Created by fengtj on 2021/9/28 8:12
+ */
+@Data
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
