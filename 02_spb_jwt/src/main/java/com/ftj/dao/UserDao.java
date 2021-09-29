55bc91cae6dc80d944037451b31e7fb8886d01bf
21seu <1693376841@qq.com>
2021-09-30 00:15:51 +0800

    bugfix

--- 02_spb_jwt/src/main/java/com/ftj/dao/UserDao.java
+++ 02_spb_jwt/src/main/java/com/ftj/dao/UserDao.java
@@ -2,8 +2,6 @@ package com.ftj.dao;
 
 import com.ftj.entity.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by fengtj on 2021/9/28 23:41
@@ -11,6 +9,5 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserDao {
 
-    @Select("select * from user where name=#{user.name} and password=#{user.password}")
-    User login(@Param("user") User user);
+    User login(User user);
 }
